4506e8e7f4b241f637f340d98a3c5f8f7bafcf65
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-08-23 16:27:41 +0530

    changes in workflow

--- 1_bmi_workflow/1_bmi_workflow.py
+++ 1_bmi_workflow/1_bmi_workflow.py
@@ -7,6 +7,7 @@ class BMIState(TypedDict):
     weight_kg:float
     height_m:float
     bmi:float
+    category:str
 
 def calculate_bmi(state:BMIState) -> BMIState:
     weight=state["weight_kg"]
@@ -15,17 +16,33 @@ def calculate_bmi(state:BMIState) -> BMIState:
     state['bmi']=round(bmi,2)
     return state
 
+def label_bmi(state:BMIState) -> BMIState:
+    bmi=state['bmi']
+    if bmi<18.5:
+        state["category"]="underweight"
+    elif 18.5<=bmi<25:
+        state['category']="Normal"
+    elif 25<=bmi<30:
+        state['category']="Overweight"
+    else:
+        state['category']="Obese"
+
+    return state
+
+
 # define your graph
 graph=StateGraph(BMIState)
 
 
 #add nodes to your graph
 graph.add_node('calculate_bmi',calculate_bmi)
+graph.add_node('label_bmi',label_bmi)
 
 
 #add edges to your graph
 graph.add_edge(START,'calculate_bmi')
-graph.add_edge('calculate_bmi',END)
+graph.add_edge('calculate_bmi','label_bmi')
+graph.add_edge('label_bmi',END)
 
 
 #compile the graph
